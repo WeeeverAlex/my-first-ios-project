960427e09d01af3c3f868d7c988fe5cd6979b994
Alexandre Wever <89090868+WeeeverAlex@users.noreply.github.com>
2022-12-30 22:19:18 -0300

    Add files via upload

---
+++ FirstProject/FirstProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by Alex Wever on 29/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPink // setting the color pink to the background
+        
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Function Color Change
+
+}
+
